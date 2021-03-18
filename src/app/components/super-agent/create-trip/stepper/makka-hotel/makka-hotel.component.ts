055faffee2810625736317ce7a58e5e375b7fa53
Sajan V <sajan@flogic.in>
2021-03-18 17:08:44 +0530

    added makka and medinah coponents

---
+++ src/app/components/super-agent/create-trip/stepper/makka-hotel/makka-hotel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-makka-hotel',
+  templateUrl: './makka-hotel.component.html',
+  styleUrls: ['./makka-hotel.component.scss']
+})
+export class MakkaHotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
