c4a3636e3ad544ae43feb5fb8c22d6615fbb4999
sabeel <you@example.com>
2021-03-19 10:30:31 +0530

    payment

--- src/app/components/super-agent/create-trip/stepper/payment/payment.component.ts
+++ src/app/components/super-agent/create-trip/stepper/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
+  advancepayment : FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
   }
 
+  validation(){
+    this.advancepayment = this.formBuilder.group({
+      adultcost: ['', Validators.required],
+      bedkidcost: ['', Validators.required],
+      kidcost: ['', Validators.required],
+      advance: ['', Validators.required],
+    })
+  }
+
+  get f(){return this.advancepayment.controls}
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.advancepayment.invalid) {
+        return;
+    }
+  }
 }
