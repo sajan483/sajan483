18bbd66a1f032899baab11e7d356b770bd9fc2fe
Sajan V <sajan@flogic.in>
2021-04-10 14:23:39 +0530

    test

--- src/app/modules/branch/branch.module.ts
+++ src/app/modules/branch/branch.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BranchRouteModule } from './landing-routing.module';
 import { HomeComponent } from 'src/app/components/branch/home/home.component';
 
+
 @NgModule({
   declarations: [
     HomeComponent
