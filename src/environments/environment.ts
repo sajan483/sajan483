3157be089b2e9a7378bf4acf1859ffc8b0a84997
sabeel <you@example.com>
2021-04-22 20:02:48 +0530

    build error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   countryCodeCommen : '91',
   timeLeft : 0,
   selectedCountryCommen : 'India',
+  
   baseUrl:'https://b2b.betatest.akbarumrah.com/apis/',
   baseUrl2:'https://b2ctest.akbarumrah.com/apis/'
   //  baseUrl:'http://192.168.10.79:8000/',
