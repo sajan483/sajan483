332aea6ae80fdbcac96f02e57008aa983ca0e451
salinimahendran <salini.mahendran@benzyinfotech.com>
2021-05-20 15:41:52 +0530

    hootelArabicTranslate

--- src/app/components/sub-agent/create-trip/components/makka-hotel/makka-hotel.component.ts
+++ src/app/components/sub-agent/create-trip/components/makka-hotel/makka-hotel.component.ts
@@ -9,6 +9,7 @@ import { NotificationService } from "src/app/common/services/notification.servic
 import { AppStore } from "src/app/stores/app.store";
 import { GeneralHelper } from "src/app/helpers/General/general-helpers";
 import { SubAgentApiService } from "src/app/Services/sub-agent-api-services";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: "app-makka-hotel",
@@ -61,7 +62,8 @@ export class MakkaHotelComponent implements OnInit,DoCheck{
     private datepipe: DatePipe,
     private router: Router,
     private helperService: HelperService,
-    private genHelper:GeneralHelper
+    private genHelper:GeneralHelper,
+    private translate: TranslateService
   ) {
     this.generalHelper = genHelper;
   }
@@ -115,6 +117,16 @@ export class MakkaHotelComponent implements OnInit,DoCheck{
       this.fetchSelectedHotelInfo(item);
   }
 
+
+  ngAfterViewChecked() {
+    this.translate.use((sessionStorage.getItem('userLanguage') === 'ar-AE') ? "ar-AE" : "en-US");
+    if (sessionStorage.getItem('userLanguage') == "ar-AE") {
+      (<HTMLInputElement>document.getElementById("body")).classList.add('mirror_css');
+    } else {
+      (<HTMLInputElement>document.getElementById("body")).classList.remove('mirror_css');
+    }
+  }
+  
   /*
    * this method for setting lang and currency
    */
