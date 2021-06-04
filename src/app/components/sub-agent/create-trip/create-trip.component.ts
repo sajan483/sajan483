7d36f27a73506319b57ab67423d610e217805b83
Sajan V <sajan@flogic.in>
2021-06-04 11:05:21 +0000

    Revert "Hlt prc sort (pull request #216)"

--- src/app/components/sub-agent/create-trip/create-trip.component.ts
+++ src/app/components/sub-agent/create-trip/create-trip.component.ts
@@ -954,7 +954,7 @@ export class CreateTripComponent implements OnInit, AfterViewChecked, DoCheck {
           }
           this.common.getHotelList(data.search_id, this.selectedCurrency, "en-US").subscribe(
             (data) => {
-              this.hotelsList = data.sort((a,b)=>(a.amount) - (b.amount));
+              this.hotelsList = data;
               if (this.hotelsList.length == 0) {
                 Swal.fire({
                   icon: 'error',
