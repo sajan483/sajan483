ba61236b7dff63ef9c8df5c049f678219c570db5
sajanv483 <sajanv483@gmail.com>
2021-06-23 21:24:12 +0530

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   countryCodeCommen : '91',
   timeLeft : 0,
   selectedCountryCommen : 'India',
-  baseUrl:'https://b2b.betatest.akbarumrah.com/apis/',
-  baseUrl2:'https://b2b.betatest.akbarumrah.com/apis/b2b/'
-  // baseUrl:'https://testb2b.akbarumrah.com/apis/',
-  // baseUrl2:'https://testb2b.akbarumrah.com/apis/b2b/'
+  // baseUrl:'https://b2b.betatest.akbarumrah.com/apis/',
+  // baseUrl2:'https://b2b.betatest.akbarumrah.com/apis/b2b/'
+  baseUrl:'https://testb2b.akbarumrah.com/apis/',
+  baseUrl2:'https://testb2b.akbarumrah.com/apis/b2b/'
 };
